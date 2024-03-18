4507985aee6b4998c2bb26c7af701c1c178365fd
Denys Nikolaichuk <>
2024-03-18 13:25:46 +0100

    First commit

---
+++ CryptoAppSwiftUI/CryptoAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Denys Nikolaichuk on 18.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoAppSwiftUI/CryptoAppSwiftUI/CryptoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppSwiftUIApp.swift
+//  CryptoAppSwiftUI
+//
+//  Created by Denys Nikolaichuk on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
